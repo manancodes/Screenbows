c11c700f913861f0e57ae4baaf925f909ce3f830
Manan Sadana <manan.sadana@cnhteam.onmicrosoft.com>
2023-06-01 21:31:16 +0530

    Feature:Download Image

--- src/Components/Console/index.js
+++ src/Components/Console/index.js
@@ -1,10 +1,24 @@
 import React from "react";
 import styles from "./console.module.css";
 import { parseDisplayValue } from "../../Utilities/UtilityFunctions";
+import domtoimage from "dom-to-image";
+import { saveAs } from "file-saver";
 
 export default function Console(props) {
   var { primaryColor, getRandomColors } = props;
-
+  const downloadHandler = (event) => {
+    event.preventDefault();
+    console.log(primaryColor.length, primaryColor);
+    if (primaryColor.length !== 7) {
+      alert("Please enter a valid Hex");
+      return;
+    }
+    domtoimage
+      .toBlob(document.getElementById("image"))
+      .then((blob) =>
+        saveAs(blob, `${primaryColor.toUpperCase()}_Background.jpg`)
+      );
+  };
   return (
     <div className={styles.console}>
       <div className={styles.colorControls}>
@@ -34,6 +48,9 @@ export default function Console(props) {
             />
           </div>
         </div>
+        <button onClick={downloadHandler} className={styles.downloadBtn}>
+          DOWNLOAD
+        </button>
       </div>
     </div>
   );
