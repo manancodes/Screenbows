4433ce8ad1f14eef13c97adc8ca78564ef0d6771
Rahmat Akintola <akintolarahmah@gmail.com>
2023-10-30 12:22:51 +0000

    Deploy to GitHub Pages

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@
 echo "Script executed successfully until this point."
 
 # Deploy to GitHub Pages
-git config --global user.name "${{ github.actor }}"
-git config --global user.email "${{ github.actor }}@users.noreply.github.com"
+git config --global user.name "$GITHUB_ACTOR"
+git config --global user.email "$GITHUB_ACTOR@users.noreply.github.com"
 git checkout gh-pages
 mv build/* ./
 git add .
